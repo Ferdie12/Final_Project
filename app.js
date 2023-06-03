f32b3e4d58ed2cd9e3bbdbcca595d2795d21a8f1
Ferdie12 <maulanaferdie1@gmail.com>
2023-06-03 19:34:01 +0700

    start project template

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const router = require('./routes');
+const cors = require('cors');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+
+app.use(router);
+
+// 404 middleware
+app.use((req,res,next) => {
+    try {
+        return res.status(404).json({
+            status: false,
+            message: "salah link uyy"
+        });
+    } catch (error) {
+        next(err);
+    }
+})
+
+// 500 middleware
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.status(500).json({
+        status: false,
+        message: err.message,
+        data: null
+    });
+});
+
+module.exports = app;
