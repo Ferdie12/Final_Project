30c7f21b64f75cd6507843ee03d6c89883ec0373
farizalju <farizaljustian@gmail.com>
2023-06-08 17:35:57 +0700

    Feat: Endpoint Create User dan Login

--- app.js
+++ app.js
@@ -4,29 +4,52 @@ const app = express();
 const morgan = require('morgan');
 const router = require('./routes');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
+const passport = require('./utils/passport');
+const session = require('express-session');
+const flash = require('express-flash');
 
+const {
+    SENTRY_DSN,
+    ENVIRONMENT,
+    JWT_SECRET_KEY
+} = process.env;
+
+Sentry.init({
+    environment: ENVIRONMENT,
+    dsn: SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({tracing: true}),
+        new Sentry.Integrations.Express({app}),
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ],
+    tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
+
+app.use(session({
+    secret: JWT_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 app.set('view engine', 'ejs');
 
 app.use(router);
 
-// 404 middleware
-app.use((req,res,next) => {
-    try {
-        return res.status(404).json({
-            status: false,
-            message: "salah link uyy"
-        });
-    } catch (error) {
-        next(err);
-    }
-})
+// Sentry error handler
+app.use(Sentry.Handlers.errorHandler());
 
-// 500 middleware
+// 500
 app.use((err, req, res, next) => {
     console.log(err);
     return res.status(500).json({
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const user = require('../controllers/user');
 
-router.get('/', (req,res) => {
-    return res.status(200).json({
-        status: true,
-        message: "Welcome to final project binar academy"
-    })
+const middlewares = require('../utils/middlewares');
+
+router.get('/', (req, res) => {
+    return res.render('index');
 });
+
+router.get('/home', (req, res) => {
+    return res.render('home');
+});
+
+router.get('/register', user.registerPage);
+router.get('/login', user.loginPage);
+router.post('/auth/register', user.register);
+router.post('/auth/login', user.login);
+router.get('/whoami', middlewares.auth, user.whoami);
 
 module.exports = router;
