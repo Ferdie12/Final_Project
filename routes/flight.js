7656b6389252963a94f55cd4d1da002ffd05772a
pitertan <pitertan0120@gmail.com>
2023-06-11 20:31:04 +0700

    modify flight routes

--- routes/flight.js
+++ routes/flight.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const flight = require('../controller/flight.js')
+const flight = require('../controllers/flight.js')
 
 router.get('/', flight.show)
 router.post('/', flight.create)
