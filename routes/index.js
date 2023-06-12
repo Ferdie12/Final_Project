6e687a4902a7846ba4efe474cf16c892a5ce6bd2
farizalju <farizaljustian@gmail.com>
2023-06-12 20:33:13 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,12 @@ module.exports = {
 
 router.use(user);
 
-router.use(user);
-
 router.get('/', (req,res) => {
-    return res.render("home.ejs");
+    return res.status(200).json({
+        status: true,
+        message: "welcome to api quick tix application in develop"
+    })
 });
-
-router.get('/welcome', (req,res) => {
-    return res.render("welcome.ejs");
-})
-
-router.get('/login', (req,res) => {
-    return res.render("login.ejs");
-})
-
-router.get('/loging', (req,res) => {
-    return res.render("google.ejs");
-})
-
-router.get('/forgot', (req,res) => {
-    return res.render("forgot.ejs");
-})
-
-router.get('/reset', (req,res) => {
-    return res.render("reset.ejs");
-})
 
 
 module.exports = router;
