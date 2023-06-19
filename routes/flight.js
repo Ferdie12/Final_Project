581c2c583e9e5f050ab19054bed3e066a11f75f8
Ferdie12 <maulanaferdie1@gmail.com>
2023-06-19 23:38:21 +0700

    change routes flights

--- routes/flight.js
+++ routes/flight.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const flight = require('../controllers/admin/flight.js')
 
-router.get('/api/flight', flight.show)
-router.post('/api/flight', flight.search)
-router.get('/api/flight/:id', flight.showOne)
-router.put('/api/flight/:id', flight.update)
-router.delete('/api/flight/:id', flight.destroy)
+router.get('/flight', flight.show)
+router.post('/flight', flight.search)
+router.get('/flight/:id', flight.showOne)
+router.put('/flight/:id', flight.update)
+router.delete('/flight/:id', flight.destroy)
 
 module.exports = router
