7128486f3817f2b80c91ab7502d155e9ff0d1183
Ferdie12 <maulanaferdie1@gmail.com>
2023-06-25 20:16:23 +0700

    modify seeder

---
+++ prisma/migrations/20230625130954_create_models/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "airplane" ALTER COLUMN "id" DROP DEFAULT;
+DROP SEQUENCE "airplane_id_seq";
--- prisma/seed/index.js
+++ prisma/seed/index.js
@@ -7,7 +7,6 @@ function insertData(){
     data_airline();
     data_airplane();
     data_airport();
-    data_flight();
 }
 
-module.exports = insertData;
+module.exports = {insertData, data_flight};
